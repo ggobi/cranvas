8e75d41d16b548e000d2b7dd9db7a077b49c6ee9
Yihui Xie <xie@yihui.name>
2010-12-31 16:56:12 -0600

    the library() calls are unnecessary; see Issue 1: https://github.com/ggobi/cranvas/issues#issue/1

--- inst/examples/nrc-ex.R
+++ inst/examples/nrc-ex.R
@@ -1,8 +1,5 @@
 ## Looking at the NRC rankings data with cranvas
 
-library(qtbase)
-library(qtpaint)
-library(plumbr)
 library(cranvas)
 
 # scatterplots of ratings variables, 5th vs 95th percentiles
@@ -19,7 +16,7 @@ data_selector(qnrc, "Institution.Name", "RGtk2")
 
 qscatter(qnrc, R.Rankings.5th.Percentile~R.Rankings.95th.Percentile)
 qscatter(qnrc, S.Rankings.5th.Percentile~S.Rankings.95th.Percentile)
-         
+
 median.centering<-function(x) {
   x<-(x-min(x, na.rm=T))/(max(x, na.rm=T)-min(x, na.rm=T))
   x<-x-median(x, na.rm=T)
