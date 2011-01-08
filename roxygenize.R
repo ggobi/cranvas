d3bcdcbd8d63f9bd983e3e118297e7ebcffe579a
Yihui Xie <xie@yihui.name>
2011-01-08 17:38:50 -0600

    a new script roxygenize.R: batch-processing roxygen; use 'Rscript roxygenize.R' to run roxygen on cranvas (requires Rd2roxygen 0.1-5 and formatR 0.1-7)

---
+++ roxygenize.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+## this script will automatically run roxygen on cranvas; it can be used as a shell script
+## e.g. Rscript roxygenize.R or Rscript roxygenize.R install
+## if there is an option 'install', this script will try to install the cranvas package
+
+## make sure the working directory is under cranvas
+if (!('cranvas' %in% list.files('../')))
+    stop('the cranvas package not found under ', normalizePath(file.path(getwd(), '..')))
+
+## requires Rd2roxygen and formatR (don't ask why; just do it)
+if (!require('Rd2roxygen')) install.packages('Rd2roxygen')
+if (!require('formatR')) install.packages('formatR')
+
+## remove the man directory first
+unlink('man', recursive = TRUE)
+
+## go up a level
+owd = setwd('..')
+
+library(Rd2roxygen)
+
+## run roxygen and several cleaning up steps
+rab('cranvas', 'cranvas', install = ('install' %in% commandArgs(TRUE)),
+    copy.package = FALSE)
+
+setwd(owd)
