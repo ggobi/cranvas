b9831009382ef88c159c43e47c60d6e3a50929e9
Yihui <xie@yihui.name>
2011-01-23 10:25:14 -0600

    get of the unnecessary 'else' statement and indents

--- R/link.R
+++ R/link.R
@@ -15,27 +15,27 @@ link = function(mf1, mf2) {
     if (!is.mutaframe(mf1) || !is.mutaframe(mf2) || is.null(link1 <- link_var(mf1)) || is.null(link2 <- link_var(mf2))) {
         stop('both mf1 and mf2 must be mutaframes and have a linking variable specified respectively',
              '\n  see ?link_var')
-    } else {
-        if (sum('.brushed' == c(colnames(mf1), colnames(mf2))) != 2) {
-            stop("both mf1 and mf2 must have a column '.brushed' respectively",
-                 '\n  see ?qmutaframe')
-        } else {
-            add_listener(mf1, function(i, j) {
-                if (focused(mf1)) {
-                    ## mf1 changed --> query link1 --> match link2 --> change mf2$.brushed
-                    ulink1 = unique(mf1[, link1][mf1$.brushed])
-                    ## update mf2$.brushed according to mf1's selected categories
-                    mf2$.brushed = mf2[, link2] %in% ulink1
-                }
-            })
-            add_listener(mf2, function(i, j) {
-                if (focused(mf2)) {
-                    ulink2 = unique(mf2[, link2][mf2$.brushed])
-                    mf1$.brushed = mf1[, link1] %in% ulink2
-                }
-            })
-        }
     }
+    if (sum('.brushed' == c(colnames(mf1), colnames(mf2))) != 2) {
+        stop("both mf1 and mf2 must have a column '.brushed' respectively",
+             '\n  see ?qmutaframe')
+    }
+    add_listener(mf1, function(i, j) {
+        if (focused(mf1)) {
+            ## mf1 changed --> query link1 --> match link2 --> change mf2$.brushed
+            ulink1 = unique(mf1[, link1][mf1$.brushed])
+            ## update mf2$.brushed according to mf1's selected categories
+            mf2$.brushed = mf2[, link2] %in% ulink1
+        }
+    })
+    add_listener(mf2, function(i, j) {
+        if (focused(mf2)) {
+            ulink2 = unique(mf2[, link2][mf2$.brushed])
+            mf1$.brushed = mf1[, link1] %in% ulink2
+        }
+    })
+
+
 }
 
 ##' Set or query the linking variable in a mutaframe.
