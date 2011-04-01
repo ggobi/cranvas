3424928b8ef8058366d391e574a756f01d13d1bf
Yihui Xie <xie@yihui.name>
2011-04-01 17:03:50 -0500

    format all the R code under this directory

---
+++ formatR.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+p = grep('--file=', commandArgs(), fixed = TRUE, value = TRUE)
+if (length(p) == 1) {
+    p = dirname(sub('^--file=', '', p))
+} else p = getwd()
+
+if (!require('formatR')) install.packages('formatR', repos = 'http://cran.r-project.org')
+
+tidy.dir(p, recursive = TRUE, keep.blank.line = TRUE)
