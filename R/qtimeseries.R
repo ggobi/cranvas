59bef84e8d5cf66202765e8a100decda9c863c42
Dianne Cook <visnut@gmail.com>
2011-04-01 11:48:08 -0500

    rearrangement of code

--- R/qtimeseries.R
+++ R/qtimeseries.R
@@ -107,9 +107,3 @@ qts <- function(time, y, data) {
     view <- qplotView(scene = scene)
     view
 }
-
-data(nasa)
-nasa11 <- subset(nasa, Gridx==1 & Gridy==1)
-qnasa <- qdata(nasa11)
-# selected(qnasa)[1] <- TRUE
-qts("TimeIndx", "ts", qnasa) 
