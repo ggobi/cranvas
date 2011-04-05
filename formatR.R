06dfad7ca35c87229ab0f56762f70be2ad8ec34c
Yihui Xie <xie@yihui.name>
2011-04-04 21:44:24 -0500

    set code line width to 80

--- formatR.R
+++ formatR.R
@@ -7,4 +7,4 @@ if (length(p) == 1) {
 
 if (!require('formatR')) install.packages('formatR', repos = 'http://cran.r-project.org')
 
-tidy.dir(p, recursive = TRUE, keep.blank.line = TRUE)
+tidy.dir(p, recursive = TRUE, keep.blank.line = TRUE, width.cutoff = 80)
