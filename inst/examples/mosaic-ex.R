c20250e7e0b417aa1f603d27b36f39a0f14b2bec
Heike Hofmann <hofmann@iastate.edu>
2011-04-19 20:31:46 -0500

    full titanic example

--- inst/examples/mosaic-ex.R
+++ inst/examples/mosaic-ex.R
@@ -11,5 +11,10 @@ data(Titanic)
 titanic <- as.data.frame(Titanic)
 qtitanic <- qdata(titanic)
 
-print(qmosaic(qtitanic, Freq ~ Sex + Age | Class, mosaic()))
 print(qmosaic(qtitanic, Freq ~ Survived, "hbar")) 
+print(qmosaic(qtitanic, Freq ~ Age, "hbar")) 
+print(qmosaic(qtitanic, Freq ~ Sex, "hbar")) 
+print(qmosaic(qtitanic, Freq ~ Class, "hbar")) 
+
+
+print(qmosaic(qtitanic, Freq ~ Sex + Age | Class, mosaic()))
