7129ed785211bf4b240127d3bc41d2990dfb31c3
xiaoyue <xiaoyue@cheng.(none)>
2011-04-29 10:18:22 -0500

    Add background and some event handlers.

--- R/qtime.R
+++ R/qtime.R
@@ -1,7 +1,45 @@
-qtime <- function(time,y,data){
-  tmpdata <- mutaframe(time=data[,time],zoomgroup=rep(1,nrow(data)))
-  cy <- y[1]
+qtime <- function(time,y,data,size=2,alpha=1,aspect.ratio=NULL){
+  
+#####################
+  ## data processing ##----------
+#####################
+  
+  arguments <- as.list(match.call()[-1])
+  df <- data.frame(data)
+  x <- eval(arguments$time, df)
+  y <- eval(arguments$y, df)
+  tdf <- mutaframe(x=x,zg=rep(1,nrow(df))) # tdf: tmp data frame; zg: zoom group.
+  .levelX <- deparse(arguments$x)
+  .levelY <- deparse(arguments$y)
 
+  dataRanges <- c(range(tdf$x), range(y))
+  windowRanges <- dataRanges
+  lims <- qrect(windowRanges[c(1, 2)], windowRanges[c(3, 4)])
+  sy <- get_axisPos(tdf$x)
+  sx <- get_axisPos(y)
+
+  ## parameters for datalayer
+  .radius <- size
+  .alpha <- alpha
+  ## parameters event handling
+  .startBrush <- NULL
+  .endBrush <- NULL
+  ## whether in the brush mode
+  .brush <- TRUE
+  ## mouse position
+  .bpos <- c(NA, NA)
+  ## drag start
+  .bstart <- c(NA, NA)
+  ## move brush?
+  .bmove <- TRUE
+  ## brush range: horizontal and vertical
+  .brange <- c(diff(windowRanges[c(1, 2)]), diff(windowRanges[c(3, 4)]))/30
+
+  
+####################
+  ## event handlers ##----------
+####################
+  
   brush_mouse_press <- function(layer, event) {
     .bstart <<- as.numeric(event$pos())
     if (event$button() == Qt$Qt$RightButton) {
@@ -10,201 +48,275 @@ qtime <- function(time,y,data){
     if (event$button() == Qt$Qt$LeftButton) {
       .bmove <<- TRUE
     }
-    if (fisheye_toggle) {
-      tmp_fisheye_bars <<- sort(fisheye_bars, decreasing=FALSE)
-      fisheye_bars <<- .bstart[c(1,1)]+c(0,0.5)
-      qupdate(main_layer)
-    }
   }
 
   brush_mouse_release <- function(layer, event){
     .bend <- as.numeric(event$pos())
-    if (!fisheye_toggle){
-      idx <- tmpdata$time>min(.bstart[1],.bend[1]) &
-      data[,cy]>min(.bstart[2],.bend[2]) &
-      tmpdata$time<max(.bstart[1],.bend[1]) &
-      data[,cy]<max(.bstart[2],.bend[2])
-      selected(data) <- idx
-      if (length(idx)) qupdate(brush_layer)
-    } else {
-      if (.bend[1]<=.bstart[1]){
-        fisheye_bars[1] <<- .bend[1]
-        fisheye_bars[2] <<- tmp_fisheye_bars[2]
-        #fisheye_bars <<- sort(fisheye_bars, decreasing=FALSE)
-        if (.bstart[1]>=tmp_fisheye_bars[2]){
-          left <- which(tmpdata$time<.bstart[1])
-          right <- which(tmpdata$time>=.bstart[1])
-          center <- NULL
-          tmpdata$time[left] <- (data[left,time]-min(data[left,time]))/(.bstart[1]-min(data[left,time]))*(.bend[1]-min(data[left,time]))+min(data[left,time])
-          tmpdata$time[right] <- (data[right,time]-.bstart[1])/(max(data[right,time])-.bstart[1])*(max(data[right,time])-.bend[1])+.bstart[1]
-        } else {
-          left <- which(tmpdata$time<.bstart[1])
-          right <- which(tmpdata$time>fisheye_bars[2])
-          center <- which(tmpdata$time>=.bstart[1] &
-                          tmpdata$time<=fisheye_bars[2])
-          if (length(center)==0) {
-            center <- NULL
-            tmpdata$time[left] <- (data[left,time]-min(data[left,time]))/(.bstart[1]-min(data[left,time]))*(.bend[1]-min(data[left,time]))+min(data[left,time])
-          tmpdata$time[right] <- (data[right,time]-fisheye_bars[2])/(max(data[right,time])-fisheye_bars[2])*(max(data[right,time])-.bend[1])+fisheye_bars[2]
-          } else {
-            tmpdata$time[left] <- (data[left,time]-min(data[left,time]))/(.bstart[1]-min(data[left,time]))*(fisheye_bars[1]-min(data[left,time]))+min(data[left,time])
-            tmpdata$time[right] <- (data[right,time]-fisheye_bars[2])/(max(data[right,time])-fisheye_bars[2])*(max(data[right,time])-fisheye_bars[2])+fisheye_bars[2]
-            tmpdata$time[center] <- (data[center,time]-.bstart[1])/(fisheye_bars[2]-.bstart[1])*(fisheye_bars[2]-fisheye_bars[1])+fisheye_bars[1]
-          }
-        }
-      } else {
-        fisheye_bars[2] <<- .bend[1]
-        fisheye_bars[1] <<- tmp_fisheye_bars[1]
-        #fisheye_bars <<- sort(fisheye_bars, decreasing=FALSE)
-        if (.bstart[1]<=tmp_fisheye_bars[1]){
-          left <- which(tmpdata$time<=.bstart[1])
-          right <- which(tmpdata$time>.bstart[1])
-          center <- NULL
-          tmpdata$time[left] <- (data[left,time]-min(data[left,time]))/(.bstart[1]-min(data[left,time]))*(.bend[1]-min(data[left,time]))+min(data[left,time])
-          tmpdata$time[right] <- (data[right,time]-.bstart[1])/(max(data[right,time])-.bstart[1])*(max(data[right,time])-.bend[1])+.bstart[1]
-        } else {
-          left <- which(tmpdata$time<fisheye_bars[1])
-          right <- which(tmpdata$time>.bstart[1])
-          center <- which(tmpdata$time>=fisheye_bars[1] &
-                          tmpdata$time<=.bstart[1])
-          if (length(center)==0) {
-            center <- NULL
-            tmpdata$time[left] <- (data[left,time]-min(data[left,time]))/(.bstart[1]-min(data[left,time]))*(.bend[1]-min(data[left,time]))+min(data[left,time])
-          tmpdata$time[right] <- (data[right,time]-.bstart[1])/(max(data[right,time])-.bstart[1])*(max(data[right,time])-.bend[1])+.bend[1]
-          } else {
-            tmpdata$time[left] <- (data[left,time]-min(data[left,time]))/(fisheye_bars[1]-min(data[left,time]))*(fisheye_bars[1]-min(data[left,time]))+min(data[left,time])
-            tmpdata$time[right] <- (data[right,time]-.bstart[1])/(max(data[right,time])-.bstart[1])*(max(data[right,time])-.bend[1])+.bend[1]
-            tmpdata$time[center] <- (data[center,time]-fisheye_bars[1])/(.bstart[1]-fisheye_bars[1])*(.bend[1]-fisheye_bars[1])+fisheye_bars[1]
-          }
-        }
-      }
-      main_layer$setLimits(qrect(range(tmpdata$time),
-                       range(data[,cy])))
-      brush_layer$setLimits(qrect(range(tmpdata$time),
-                       range(data[,cy])))
-      qupdate(main_layer)
-      qupdate(xaxis)
+    idx <- tdf$x>min(.bstart[1],.bend[1]) &
+    y>min(.bstart[2],.bend[2]) &
+    tdf$x<max(.bstart[1],.bend[1]) &
+    y<max(.bstart[2],.bend[2])
+    selected(data) <- idx
+    if (length(idx)) qupdate(brush_layer)
+  }
+
+  identify_mouse_move <- function(layer, event) {
+    pos <- event$pos()
+    .bpos <<- as.numeric(pos)
+    ## simple click: don't change .brange
+    if (!all(.bpos == .bstart) && (!.bmove)) {
+      .brange <<- .bpos - .bstart
     }
-  }
+    .new.brushed <- rep(FALSE, nrow(data))
+    xrange <- .radius/root_layer$size$width() * diff(windowRanges[c(1, 2)])
+    yrange <- .radius/root_layer$size$height() * diff(windowRanges[c(3, 4)])
 
-  mouse_double_click <- function(layer, event){
-  #  fisheye_toggle <<-!fisheye_toggle
-  #  tmpdata$time <- data[,time]
-  #  tmpdata$zoomgroup <- rep(1,nrow(data))
-  #  if (fisheye_toggle){
-   #   fisheye_bars <<- c(0.3,0.7)*(max(tmpdata$time)-min(tmpdata$time))+min(tmpdata$time)
-  #  }
-  #  main_layer$setLimits(qrect(range(tmpdata$time),
-   #                    range(data[,cy])))
-  #  brush_layer$setLimits(qrect(range(tmpdata$time),
-   #                    range(data[,cy])))
-  #  qupdate(main_layer)
-  #  qupdate(xaxis)
-  }
+    rect <- qrect(matrix(c(.bpos - .brange - c(xrange, yrange), .bpos + .brange + c(xrange, yrange)), 2, byrow = TRUE))
 
-  mouse_wheel <- function(layer, event){
-   # tmp<<-event
-   # print(event$type())
+    hits <- layer$locate(rect) + 1
+        
+    .new.brushed[hits] <- TRUE
+    data$.brushed <- mode_selection(data$.brushed, .new.brushed, mode = brush(data)$mode)
   }
 
   key_press <- function(layer, event){
-    crt_range <- max(tmpdata$time)-min(tmpdata$time)+1
+    crt_range <- max(tdf$x)-min(tdf$x)+1
+
     if (event$key()==Qt$Qt$Key_Plus){
+      ## plus
       zoombound=min(round(0.96*crt_range),crt_range-1)
       if (zoombound<3)zoombound <- 3
-      tmpdata$time <- data[,time]%%zoombound
-      tmpdata$zoomgroup <- ceiling(data[,time]/zoombound)
-      if (sum(tmpdata$time==0)){
-        tmpdata$zoomgroup[tmpdata$time==0] <- tmpdata$zoomgroup[which(tmpdata$time==0)-1]
-        tmpdata$time[tmpdata$time==0] <- zoombound
+      tdf$x <- x%%zoombound
+      tdf$zg <- ceiling(x/zoombound)
+      if (sum(tdf$x==0)){
+        tdf$zg[tdf$x==0] <- tdf$zg[which(tdf$x==0)-1]
+        tdf$x[tdf$x==0] <- zoombound
       }
+      dataRanges <<- c(range(tdf$x), range(y))
+      windowRanges <<- dataRanges
+      sy <<- get_axisPos(tdf$x)
+      sx <<- get_axisPos(y)
+      lims <<- qrect(windowRanges[c(1, 2)], windowRanges[c(3, 4)])
+      qupdate(bg_layer)
+      main_layer$setLimits(lims)
+      brush_layer$setLimits(lims)
     }
+
     if (event$key()==Qt$Qt$Key_Minus){
+      ## minus
       zoombound=max(round(crt_range*25/24),crt_range+1)
-      if (zoombound>(max(data[,time])-min(data[,time])+1)) zoombound <- max(data[,time])-min(data[,time])+1
-      tmpdata$time <- data[,time]%%zoombound
-      tmpdata$zoomgroup <- ceiling(data[,time]/zoombound)
-      if (sum(tmpdata$time==0)){
-        tmpdata$zoomgroup[tmpdata$time==0] <- tmpdata$zoomgroup[which(tmpdata$time==0)-1]
-        tmpdata$time[tmpdata$time==0] <- zoombound
+      if (zoombound>(max(x)-min(x)+1)) zoombound <- max(x)-min(x)+1
+      tdf$x <- x%%zoombound
+      tdf$zg <- ceiling(x/zoombound)
+      if (sum(tdf$x==0)){
+        tdf$zg[tdf$x==0] <- tdf$zg[which(tdf$x==0)-1]
+        tdf$x[tdf$x==0] <- zoombound
       }
+      dataRanges <<- c(range(tdf$x), range(y))
+      windowRanges <<- dataRanges
+      sy <<- get_axisPos(tdf$x)
+      sx <<- get_axisPos(y)
+      lims <<- qrect(windowRanges[c(1, 2)], windowRanges[c(3, 4)])
+      qupdate(bg_layer)
+      main_layer$setLimits(lims)
+      brush_layer$setLimits(lims)
     }
-    if (event$key()==Qt$Qt$Key_Up){
-      fisheye_toggle <<-!fisheye_toggle
-      tmpdata$time <- data[,time]
-      tmpdata$zoomgroup <- rep(1,nrow(data))
-      if (fisheye_toggle){
-        fisheye_bars <<- c(0.3,0.7)*(max(tmpdata$time)-min(tmpdata$time))+min(tmpdata$time)
-      }
+
+    if (event$key() == Qt$Qt$Key_Up) {
+      ## arrow up
+      .radius <<- .radius + 1
     }
-    main_layer$setLimits(qrect(range(tmpdata$time),
-                       range(data[,cy])))
-    brush_layer$setLimits(qrect(range(tmpdata$time),
-                       range(data[,cy])))
+
+    if (event$key() == Qt$Qt$Key_Down & .radius > 0) {
+      ## arrow down
+      .radius <<- .radius - 1
+    }
+
+    if (event$key() == Qt$Qt$Key_Right & .alpha < 1) {
+      ## arrow right
+      .alpha <<- 1.1 * .alpha
+      main_layer$setOpacity(.alpha)
+    }
+
+    if (event$key() == Qt$Qt$Key_Left & .alpha > 1/nrow(data)) {
+      ## arrow left
+      .alpha <<- 0.9 * .alpha
+      main_layer$setOpacity(.alpha)
+    }
     qupdate(main_layer)
-    qupdate(xaxis)
+    ## qupdate(xaxis)
   }
 
+  ## Display category information on hover (query) ----------------------
+  .queryPos <- NULL
+    
+  query_draw <- function(item, painter, exposed, ...) {
+    if (is.null(.queryPos)) return()
+        
+    xpos <- .queryPos[1]
+    ypos <- .queryPos[2]
+        
+    xrange <- .radius/root_layer$size$width() * diff(windowRanges[c(1, 2)])
+    yrange <- .radius/root_layer$size$height() * diff(windowRanges[c(3, 4)])
+                                          print(xrange)
+                                          print(yrange)
+    rect <- qrect(matrix(c(xpos - xrange, ypos - yrange, xpos + xrange, ypos + yrange), 2, byrow = TRUE))
+    hits <- main_layer$locate(rect) + 1
+                                        #print(hits)
+                                        print(.queryPos)
+                                        #browser()
+    ## Nothing under mouse?
+    if (length(hits) == 0) return()
+        
+    info <- as.data.frame(data[hits, c(.levelX, .levelY)])
+                                        #browser()
+        
+    ## Work out label text
+    idx <- names(info)
+    if (length(hits) == 1) {
+      infodata <- as.character(unlist(info[, idx]))
+      infostring <- paste(idx, infodata, collapse = "\n", sep = ": ")
+    }
+    else {
+                                        #browser()
+      xymin <- unlist(lapply(info[, idx], min, na.rm = T))
+      xymax <- unlist(lapply(info[, idx], max, na.rm = T))
+      infostring <- paste(idx, paste(xymin, xymax, sep = " - "), collapse = "\n", 
+                          sep = ": ")
+      infostring <- paste(" xxhitsxx points\n", infostring)
+      infostring <- gsub("xxhitsxx", length(hits), infostring)
+    }
+    bgwidth <- qstrWidth(painter, infostring)
+    bgheight <- qstrHeight(painter, infostring)
+        
+    ## adjust drawing directions when close to the boundary
+    hflag <- windowRanges[2] - xpos > bgwidth
+    vflag <- ypos - windowRanges[3] > bgheight
+    qdrawRect(painter, xpos, ypos, xpos + ifelse(hflag, 1, -1) * bgwidth, ypos + ifelse(vflag, -1, 1) * bgheight, stroke = rgb(1, 1, 1), fill = rgb(1, 1, 1, 0.9))
+        
+    qstrokeColor(painter) <- brush(data)$label.color
+    qdrawText(painter, infostring, xpos, ypos, halign = ifelse(hflag, "left", "right"), valign = ifelse(vflag, "top", "bottom"))
+        
+  }
+    
+  query_hover <- function(item, event, ...) {
+    .queryPos <<- as.numeric(event$pos())
+    qupdate(querylayer)
+  }
+    
+  query_hover_leave <- function(item, event, ...) {
+    .queryPos <<- NULL
+    qupdate(querylayer)
+  }
+    
+  
+  
+############
+  ## layers ##----------
+############
+  
+  bg_draw <- function(item, painter, exposed) {
+    draw_grid_with_positions_fun(painter, dataRanges, sy, sx)
+
+    labels <- get_axisPos(tdf$x)
+    draw_x_axes_with_labels_fun(painter, dataRanges, axisLabel = sy, labelHoriPos = sy, name = NULL)
+
+    labels <- get_axisPos(y)
+    draw_y_axes_with_labels_fun(painter, dataRanges, axisLabel = sx, labelVertPos = sx, name = NULL)   
+  }
+    
   main_draw <- function(layer,painter){
-    if (fisheye_toggle){
-      qdrawRect(painter,fisheye_bars[1]-0.25,min(data[,cy]),
-                fisheye_bars[1]+0.25,max(data[,cy]),
-                stroke='grey',fill='grey')
-      qdrawRect(painter,fisheye_bars[2]-0.25,min(data[,cy]),
-                fisheye_bars[2]+0.25,max(data[,cy]),
-                stroke='grey',fill='grey')
-    }
-    color=gray(seq(0,0.8,length=max(tmpdata$zoomgroup)))
-    for (i in 1:max(tmpdata$zoomgroup)) {
-      qdrawCircle(painter,tmpdata[tmpdata$zoomgroup==i,1],
-                  data[tmpdata$zoomgroup==i,cy],2,
+    color=gray(seq(0,0.6,length=max(tdf$zg)))
+    for (i in 1:max(tdf$zg)) {
+      qdrawCircle(painter,tdf[tdf$zg==i,1],
+                  y[tdf$zg==i],r=.radius,
                   fill=color[i],stroke=color[i])
-      qdrawLine(painter,tmpdata[tmpdata$zoomgroup==i,1],
-                data[tmpdata$zoomgroup==i,cy],stroke=color[i])
+      qdrawLine(painter,tdf[tdf$zg==i,1],
+                y[tdf$zg==i],stroke=color[i])
     }
   }
+  
   brush_draw <- function(layer, painter) {
-    idx <- selected(data)
-    if (any(idx)){
-      txt <- paste("Point ",rownames(data)[idx],"\nTime: ",
-                   data[idx,time],"\nValue: ",data[idx,cy],
-                   sep="")
-      qdrawText(painter,txt,tmpdata[idx,1],data[idx,cy])
+    ## idx <- selected(data)
+    ## if (any(idx)){
+    ##   txt <- paste("Point ",rownames(df)[idx],"\nx: ",
+    ##                x[idx],"\nValue: ",y[idx],
+    ##                sep="")
+    ##   qdrawText(painter,txt,tdf[idx,1],y[idx])
+    ## }
+    .brushed = data$.brushed
+    if (.brush) {
+      if (!any(is.na(.bpos))) {
+        qlineWidth(painter) = brush(data)$size
+        qdrawRect(painter, .bpos[1] - .brange[1], .bpos[2] - .brange[2], 
+                  .bpos[1], .bpos[2], stroke = brush(data)$color)
+      }
+            
+      hdata <- subset(data.frame(tdf$x,y), .brushed)
+      print(hdata)
+            
+      if (nrow(hdata) > 0) {
+        ## draw the brush rectangle
+        if (!any(is.na(.bpos))) {
+          qlineWidth(painter) <- brush(data)$size
+          qdrawRect(painter, .bpos[1] - .brange[1], .bpos[2] - .brange[2], 
+                    .bpos[1], .bpos[2], stroke = brush(data)$color)
+        }
+        ## (re)draw brushed data points
+        brushx <- hdata$x
+        brushy <- hdata$y
+        fill = brush(data)$color
+        stroke = brush(data)$color
+        radius <- .radius
+                
+        qdrawCircle(painter, x = brushx, y = brushy, r = radius, fill = fill, stroke = stroke)
+      }
     }
 
   }
+
+  
+#####################
+  ## draw the canvas ##----------
+#####################
+
+  xWidth <- 800
+  yWidth <- 500
+  if (!is.null(aspect.ratio)) xWidth <- round(yWidth*aspect.ratio)
+  
   scene <- qscene()
   root_layer <- qlayer(scene)
-  xaxis <- qaxis(root_layer, data = tmpdata$time, side = 1,
-                 limits=range(tmpdata$time),
-                 row = 2, col = 1)
-  yaxis <- qaxis(root_layer, data = data[,cy], side = 2,
-                 limits=range(data[,cy]),
-                 row = 1, col = 0)
-  fisheye_toggle <<- FALSE
-
+  root_layer$setGeometry(qrect(0, 0, xWidth, yWidth))
+  bg_layer <- qlayer(parent = root_layer, paintFun = bg_draw, limits=lims, row = 1, col = 1)
+  ## xaxis <- qaxis(root_layer, data = tdf$x, side = 1,
+  ##                limits=range(tdf$x),
+  ##                row = 2, col = 1)
+  ## yaxis <- qaxis(root_layer, data = y, side = 2,
+  ##                limits=range(y),
+  ##                row = 1, col = 0)
   main_layer <- qlayer(root_layer,paintFun=main_draw,
                        mousePressFun=brush_mouse_press,
                        mouseReleaseFun=brush_mouse_release,
-                      # mouseDoubleClickFun=mouse_double_click,
-                       wheelFun=mouse_wheel,
+                       mouseMove = identify_mouse_move, 
                        keyPressFun=key_press,
-                       limits=qrect(range(tmpdata$time),
-                       range(data[,cy])), row = 1, col = 1)
+                       limits=lims, row = 1, col = 1)
   brush_layer <- qlayer(root_layer, brush_draw,
-                        limits=qrect(range(tmpdata$time),
-                        range(data[,cy])), row = 1, col = 1)
+                        limits=qrect(range(tdf$x),
+                        range(y)), row = 1, col = 1)
+  query_layer <- qlayer(root_layer, query_draw,
+                       limits = lims, hoverMoveFun = query_hover,
+                       hoverLeaveFun = query_hover_leave)
 
-    layout = root_layer$gridLayout()
-    layout$setRowPreferredHeight(0, 30)
-    layout$setColumnPreferredWidth(0, 30)
-    layout$setRowPreferredHeight(2, 30)
-    layout$setColumnMaximumWidth(2, 10)
-    layout$setRowStretchFactor(0, 0)
-    layout$setColumnStretchFactor(0, 0)
-    layout$setRowStretchFactor(2, 0)
+  layout = root_layer$gridLayout()
+  layout$setRowPreferredHeight(0, 10)
+  layout$setColumnPreferredWidth(0, 70)
+  layout$setRowPreferredHeight(2, 30)
+  layout$setColumnPreferredWidth(2, 10)
+  layout$setColumnMaximumWidth(2, 15)
+  layout$setRowStretchFactor(0, 0)
+  layout$setColumnStretchFactor(0, 0)
+  layout$setRowStretchFactor(2, 0)
 
   view <- qplotView(scene=scene)
   view
-
 }
