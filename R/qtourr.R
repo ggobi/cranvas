3ebfff53af918edf75fcaa8af6e50688b6f798db
Dianne Cook <visnut@gmail.com>
2011-05-11 13:24:23 -0500

    scatterplot syntax

--- R/qtourr.R
+++ R/qtourr.R
@@ -14,7 +14,8 @@ tourr <- function() {
 #' flea_tour <- Tourr$new(qflea, grand_tour(3), 1:6)
 #' flea_tour$step()
 #' qparallel(c("tour_1", "tour_2", "tour_3"), qflea)
-#' # qscatter(qflea, tour_1 ~ tour_2)
+#' qscatter(qflea, tour_1, tour_2)
+#' # qhist(qflea, "tour_1")
 #' flea_tour$start()
 #' flea_tour$pause()
 
