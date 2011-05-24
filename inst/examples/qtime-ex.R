cff8d6b3b97b68bb43e3c04d0695c046a642569d
xiaoyue <xiaoyue@cheng.(none)>
2011-05-24 02:28:06 -0500

    example to interact with scatterplot

--- inst/examples/qtime-ex.R
+++ inst/examples/qtime-ex.R
@@ -4,4 +4,6 @@ data(nasa)
 nasa11 <- subset(nasa, Gridx == 1 & Gridy == 1)
 qnasa <- qdata(nasa11)
 selected(qnasa)[1] <- TRUE
-qtime(TimeIndx, ts, qnasa) 
+qtime(TimeIndx, ts, qnasa)
+
+qscatter(qnasa,ts,tsa_tovs)
