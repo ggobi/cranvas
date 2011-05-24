a93f7a9ddd00860fa6b4e2b8173bdee35a82c598
xiaoyue <xiaoyue@cheng.(none)>
2011-05-24 11:06:02 -0500

    example for more than one variable

--- inst/examples/qtime-ex.R
+++ inst/examples/qtime-ex.R
@@ -4,6 +4,8 @@ data(nasa)
 nasa11 <- subset(nasa, Gridx == 1 & Gridy == 1)
 qnasa <- qdata(nasa11)
 selected(qnasa)[1] <- TRUE
-qtime(TimeIndx, ts, qnasa)
 
+qtime(TimeIndx, ts, qnasa)
 qscatter(qnasa,ts,tsa_tovs)
+
+qtime(TimeIndx,c(ts,ps_tovs,ca_med),qnasa)
