afe008ab11cfe3ac0e70792b45580318a9fe6f0a
Yihui Xie <xie@yihui.name>
2011-05-28 01:05:06 -0500

    tweaking examples of boxplots

--- inst/examples/qboxplot-ex.R
+++ inst/examples/qboxplot-ex.R
@@ -1,3 +1,5 @@
+library(cranvas)
+
 df = matrix(rnorm(1000), 200)
 qboxplot(df)
 qboxplot(df, horizontal = TRUE)
@@ -7,6 +9,6 @@ qboxplot(rnorm(100))
 qboxplot(Sepal.Length~Species,data=iris)
 
 df = qdata(round(data.frame(x = rnorm(100), y = runif(100), z = rbeta(100, 1, .8)), 2))
-## brush(df, 'size') = 1  # I'm unable to work with size > 1
+
 qboxplot(data = df)
-qparallel(data = df)
+qparallel(data = df)  # brush on par-coords and you see little boxplots
