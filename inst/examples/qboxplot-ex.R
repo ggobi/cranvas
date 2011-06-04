d86b9bb8ec6bce5080d83c6338e45834a014ad4f
Dianne Cook <visnut@gmail.com>
2011-06-04 06:41:57 -0500

    add a data example to boxplot

--- inst/examples/qboxplot-ex.R
+++ inst/examples/qboxplot-ex.R
@@ -1,6 +1,7 @@
 library(cranvas)
 
 df = matrix(rnorm(1000), 200)
+colnames(df) <- c("V1", "V2", "V3", "V4", "V5")
 qboxplot(df)
 qboxplot(df, horizontal = TRUE)
 qboxplot(df, at = (1:5)^2)  # at different locations
@@ -11,4 +12,8 @@ qboxplot(Sepal.Length~Species,data=iris)
 df = qdata(round(data.frame(x = rnorm(100), y = runif(100), z = rbeta(100, 1, .8)), 2))
 
 qboxplot(data = df)
-qparallel(data = df)  # brush on par-coords and you see little boxplots
+qparallel(~ ., data = df)  # brush on par-coords and you see little boxplots
+
+data(flea)
+qflea <- qdata(flea[,1:6])
+qboxplot(. ~  species, data = flea)
