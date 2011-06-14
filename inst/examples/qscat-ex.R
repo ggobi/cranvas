101abb0ccd095dbe7238ce0d7bb62355047442f8
Yihui Xie <xie@yihui.name>
2011-06-14 01:45:52 -0500

    usually we do not need to load these packages explicitly (if you are testing your code, you can use source('load.R'))

--- inst/examples/qscat-ex.R
+++ inst/examples/qscat-ex.R
@@ -1,27 +1,14 @@
-library(qtbase)
-library(qtpaint)
-library(plumbr)
 library(cranvas)
 library(scales)
 
 data(nrcstat)
 nrcstat[,26]<- -nrcstat[,26]
 colnames(nrcstat)[26]<-"Neg.Median.Time.to.Degree"
-  
+
 qnrc = qdata(nrcstat)
 
 print(qscatter(qnrc, R.Rankings.5th.Percentile, R.Rankings.95th.Percentile))
 print(qscatter(qnrc, S.Rankings.5th.Percentile, S.Rankings.95th.Percentile))
-
-
-
-#################
-#
-library(qtbase)
-library(qtpaint)
-library(plumbr)
-library(cranvas)
-library(scales)
 
 data(pollen)
 qpollen <- qdata(pollen)
