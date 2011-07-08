10a023428f1ab6ea722dfa455ed7dfba6158bd38
Dianne Cook <visnut@gmail.com>
2011-07-08 15:03:09 -0500

    another example

--- inst/examples/qdensity-ex.R
+++ inst/examples/qdensity-ex.R
@@ -10,3 +10,11 @@ print(qdensity(Second.Serve.Pts, qtennis))
 data(pollen)
 qpollen <- qdata(pollen)
 print(qdensity(RIDGE, qpollen))
+
+data(flea)
+qflea <- qdata(flea)
+
+print(qdensity(tars1, qflea))
+print(qdensity(tars2, qflea))
+print(qdensity(aede1, qflea))
+print(qdensity(aede3, qflea))
