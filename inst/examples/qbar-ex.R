b55d3d0cb1618dde576f617e57340a0f882b5f48
Yihui Xie <xie@yihui.name>
2011-07-19 14:40:10 -0500

    use the hue palette in qbar() examples

--- inst/examples/qbar-ex.R
+++ inst/examples/qbar-ex.R
@@ -1,6 +1,6 @@
 library(cranvas)
 data(nrcstat)
-qnrc = qdata(nrcstat, fill = dscale(qnrc$Regional.Code, brewer_pal(type = 'qual')))
+qnrc = qdata(nrcstat, fill = dscale(qnrc$Regional.Code, hue_pal()), color = dscale(qnrc$Regional.Code, hue_pal()))
 qbar(Regional.Code, qnrc)
 qparallel(vars = 13:10, data = qnrc, main = "Overview of Rankings", glyph = "tick",
     horizontal = FALSE, boxplot = TRUE)
