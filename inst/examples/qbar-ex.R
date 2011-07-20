7404a80f81b2ffe19d180b2ad52723eba52219e5
Yihui Xie <xie@yihui.name>
2011-07-20 14:43:31 -0500

    fixed the qbar example error

--- inst/examples/qbar-ex.R
+++ inst/examples/qbar-ex.R
@@ -1,6 +1,6 @@
 library(cranvas)
 data(nrcstat)
-qnrc = qdata(nrcstat, fill = dscale(qnrc$Regional.Code, hue_pal()), color = dscale(qnrc$Regional.Code, hue_pal()))
+qnrc = qdata(nrcstat, fill = dscale(nrcstat$Regional.Code, hue_pal()), color = dscale(nrcstat$Regional.Code, hue_pal()))
 qbar(Regional.Code, qnrc)
 qparallel(vars = 13:10, data = qnrc, main = "Overview of Rankings", glyph = "tick",
     horizontal = FALSE, boxplot = TRUE)
