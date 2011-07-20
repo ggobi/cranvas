65b0efa079dbc00320d991264ad3d996117f6f90
Dianne Cook <visnut@gmail.com>
2011-07-20 14:23:10 -0500

    syntax updates

--- inst/examples/ex-oil-spill.R
+++ inst/examples/ex-oil-spill.R
@@ -1,6 +1,3 @@
-library(qtbase)
-library(qtpaint)
-library(plumbr)
 require(stringr)
 library(cranvas)
 library(ggplot2)
@@ -20,20 +17,18 @@ floats$date <- as.Date(floats$Date_Time, format="%Y-%m-%d")
 floats$day <- as.numeric(floats$date - as.Date("2010-04-20", format="%Y-%m-%d"))
 floats$week <- (floats$day %/% 7) + 1
 
-
-
 qfl <- qdata(floats)
 qfl$.color <- "grey20"
 #brush(qfl, "color") <- "yellow"
 brush(qfl, "label.color") <- "grey30"
-qhist(data=qfl, x="Depth", binwidth=10, horizontal=FALSE)
+qhist(Depth, qfl, binwidth=10, horizontal=FALSE)
 qscatter(Longitude, Latitude, data=qfl)
 
 #
 qmosaic(data=qfl, ~callSign, "vbar")
 #
-qhist(data=qfl, xCol="day", binwidth=1, horizontal=FALSE)
-qhist(data=qfl, xCol="day", binwidth=7, horizontal=FALSE)
+qhist(day, data=qfl, binwidth=1, horizontal=FALSE)
+qhist(day, data=qfl, binwidth=7, horizontal=FALSE)
 #
 qscatter(Temperature, Salinity, data=qfl)
 
