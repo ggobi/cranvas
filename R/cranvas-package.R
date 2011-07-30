3a0bbeba73a571e387b92783acba44800e2602ac
Yihui Xie <xie@yihui.name>
2011-07-30 09:38:42 -0500

    more detailed documentation on the cranvas package, with examples

--- R/cranvas-package.R
+++ R/cranvas-package.R
@@ -1,20 +1,48 @@
-##' Interactive Statistical Graphics Based on Qt
+##' Interactive statistical graphics based on Qt
 ##'
+##' This package was designed mainly for interactions in statistical
+##' plots, a feature (nearly) missing in \R for long. It contains most
+##' common statistical plots like histograms, scatter plots, bar
+##' charts, parallel coordinates plots, density plots, mosaic plots,
+##' boxplots, maps, missing value plots and time series plots. All
+##' plots support some common interactions as well as plot-specific
+##' interactions with the keyboard or the mouse.
+##'
+##' The actual drawing is based on two packages \pkg{qtbase} and
+##' \pkg{qtpaint}, which connect \R to Qt. The data structure is based
+##' on \pkg{plumbr} and \pkg{objectSignals}; the
+##' \code{\link[plumbr]{mutaframe}}s and reference classes are used
+##' extensively in this package. Usually there are listeners
+##' (\code{\link[plumbr]{add_listener}}) and signaling fields
+##' (\code{\link[objectSignals]{signalingFields}}) attached to data
+##' objects (created by \code{\link{qdata}}), so the plots can listen
+##' to the changes in data (hence get updated). Note all the plots
+##' based on the same data object are linked by default, so the
+##' interactions in one plot will be reflected in other plots as well.
+##'
+##' See \code{\link{common_key_press}} and
+##' \code{\link{common_key_release}} for common interactions in all
+##' plots, and the documentation of specific plots for other possible
+##' interactions.
 ##' @importFrom qtbase Qt
 ##' @importFrom qtbase qrect qfont qsize qconnect qdataFrameModel
 ##' @import qtpaint
 ##' @import plumbr
 ##' @import objectSignals
 ##' @name cranvas-package
+##' @aliases cranvas
 ##' @docType package
+##' @example inst/examples/cranvas-ex.R
 NULL
 
-##' NRC Rankings Data for the Statistics Departments in US
+##' NRC rankings data for the statistics departments in the US
 ##'
-##' This data contains the NRC rankings for all the statistics departments in US.
+##' This data contains the NRC rankings for all the statistics
+##' departments in US.
 ##'
 ##' @name nrcstat
 ##' @docType data
+##' @usage data(nrcstat)
 ##' @format data.frame: 61 obs. of  68 variables
 ##' @keywords datasets
 ##' @source \url{http://sites.nationalacademies.org/pga/resdoc/index.htm}
---
+++ inst/examples/cranvas-ex.R
@@ -0,0 +1,13 @@
+library(cranvas)
+data(tennis)
+## the first step is always to create the data object
+qtennis = qdata(tennis, color = Serve.Speed, fill = Serve.Speed, size = Aces)
+
+## then create plots
+qhist2(Serve.Speed, data = qtennis)
+
+qbar(Country, data = qtennis)
+
+qparallel(~., data = qtennis)  # all variables; you may need a large window
+
+record_selector("Name", data = qtennis)  # a simple GUI
