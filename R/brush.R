b213346854e540940b95c9c2f03bb4ae892b5fee
Yihui Xie <xie@yihui.name>
2011-08-03 21:48:38 -0400

    smaller hotspot (close #68)

--- R/brush.R
+++ R/brush.R
@@ -304,6 +304,6 @@ draw_brush = function(layer, painter, data, meta) {
     qdrawRect(painter, meta$pos[1] - meta$brush.size[1],
               meta$pos[2] - meta$brush.size[2], meta$pos[1], meta$pos[2],
               stroke = b$style$color, fill = NA)
-    qdrawCircle(painter, meta$pos[1], meta$pos[2], r = 1.5 * b$style$linewidth,
+    qdrawCircle(painter, meta$pos[1], meta$pos[2], r = 1.1 * b$style$linewidth,
                 stroke = b$style$color, fill = b$style$color)
 }
