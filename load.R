f89b5132d890edc0076efb31a3382e487c6cf468
Yihui Xie <xie@yihui.name>
2011-08-26 21:07:52 -0500

    no need to use load.R; please use load_all() in devtools package (see my email)

--- load.R
+++
@@ -1,8 +0,0 @@
-## when testing your code, you may need to load these packages (feel free to
-#   add more here)
-library(qtbase)
-library(qtpaint)
-library(cranvas)
-library(plumbr)
-library(scales)
-library(objectSignals)
