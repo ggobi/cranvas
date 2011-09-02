8d99b1f89083bf751ccd1184fc41fb0e83fc79c8
Xiaoyue Cheng <chequered@gmail.com>
2011-09-02 14:28:27 -0500

    typo

--- inst/examples/qtime-ex.R
+++ inst/examples/qtime-ex.R
@@ -21,7 +21,7 @@ qnasaTsCa <- qdata(nasaTsCa)
 print(qtime(TimeIndx,~value,qnasaTsCa,group=variable,shift=c(1,12)))
 
 
-u## example 2: Remifentanil in the nlme package
+## example 2: Remifentanil in the nlme package
 require(nlme)
 Rem <- qdata(Remifentanil[complete.cases(Remifentanil) &
                           Remifentanil$ID==1,])
@@ -42,12 +42,12 @@ link_var(qRemi) <- NULL  # OFF
 ## example 3: Wages
 data(wages)
 wage <- qdata(wages[wages$id<2000,1:3])
-print(qtime(exper,~lnw,wage,group=id))
-
 wage$id <- as.factor(wage$id)
+print(qtime(exper,~lnw,wage,group=id))
 link_var(wage) <- "id"   # ON
 link_type(wage) <- "self"
 link_var(wage) <- NULL  # OFF
+
 
 ## example 4: Lynx - for posterity
 # Good to show off wrapping to investigate irregular series
@@ -55,11 +55,13 @@ data(lynx)
 qlynx<-qdata(data.frame(Time=1:114, lynx))
 print(qtime(Time, ~lynx, qlynx, shift=1:12))
 
+
 ## example 5: Sunspots - for posterity
 # Good to show off wrapping to investigate irregular series
 data(sunspots)
 qsun<-qdata(data.frame(Time=1:2820, sunspots))
 print(qtime(Time, ~sunspots, qsun, shift=c(1,(1:10)*10)))
+
 
 ## example 6: Pigs
 data(pigs)
