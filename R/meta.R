5568e9e3b24290742c61508eb506c2d5f5712843
Yihui Xie <xie@yihui.name>
2011-09-02 15:26:02 -0500

    only provide a template for common meta (close #104)

--- R/meta.R
+++ R/meta.R
@@ -7,7 +7,11 @@
 ##' several plotting routines more modularized (e.g. the axes and
 ##' background layers).
 ##'
-##' We can use \code{Common.meta$new()} to generate an object, which
+##' This object only provides a template for the common meta data, we
+##' have to call \code{\link[plumbr]{signalingFields}} in order to
+##' \emph{really} create a meta object.
+##'
+##' We can use \code{Meta.template$new()} to generate an object, which
 ##' is essentially an environment. This environment object is dynamic
 ##' in the sense that no matter where we copy it to or how we modify
 ##' the components in it, all its copies can sync with each
@@ -19,43 +23,48 @@
 ##' object. When a component is changed, an event can be triggered
 ##' (see examples below).
 ##'
-##' The common meta data contains the following components:
+##' The common meta structure contains the following components:
 ##'
-##' \describe{\item{alpha}{the alpha transparency} \item{xat, yat}{the
-##' tickmarks locations on the x and y axis} \item{xlat, ylab}{x and y
-##' axis titles} \item{xlabels, ylabels}{the tickmarks labels}
-##' \item{limits}{the limits of the main plot layer (it is often the
-##' case that some other layers need to sync with the main plot layer
-##' in limits, so this component is very important)} \item{color,
-##' border, size}{the real color, border and size vectors used to draw
-##' the graphical elements, which can be different from the
-##' corresponding columns in the original data} \item{start, pos}{the
-##' starting position and the current position of the cursor}
-##' \item{brush.move}{indicating if the brush is in the move mode (if
-##' not, the brush will be resized when mouse moves}
+##' \describe{\item{alpha}{the alpha transparency} \item{main}{the
+##' main title} \item{xat, yat}{the tickmarks locations on the x and y
+##' axis} \item{xlat, ylab}{x and y axis titles} \item{xlabels,
+##' ylabels}{the tickmarks labels} \item{limits}{the limits of the
+##' main plot layer (it is often the case that some other layers need
+##' to sync with the main plot layer in limits, so this component is
+##' very important)} \item{color, border, size}{the real color, border
+##' and size vectors used to draw the graphical elements, which can be
+##' different from the corresponding columns in the original data}
+##' \item{start, pos}{the starting position and the current position
+##' of the cursor} \item{brush.move}{indicating if the brush is in the
+##' move mode (if not, the brush will be resized when mouse moves}
 ##' \item{brush.size}{the size of the brush rectangle (a numeric
 ##' vector of length 2)} \item{manual.brush}{a function to manually
 ##' brush the plot given the mouse position} \item{identified,
 ##' identify.labels}{the identified indices and the text label to draw
 ##' in the plot for the identified cases}}
-##' @examples meta1 = Common.meta$new(alpha = 1); meta1$alpha; meta$xat = 1:5
-##' meta1$pos = c(1, 4)
+##' @examples library(objectSignals)
+##' My.meta = setRefClass("My_meta", fields = signalingFields(c(Common.meta,
+##' list(horizontal = 'logical', xleft = 'numeric'))))
+##' meta = My.meta$new(alpha = 1); meta$alpha; meta$xat = 1:5
+##' meta$pos = c(1, 4); meta$horizontal = FALSE
 ##'
-##' meta2 = meta1; meta2$xat = 2:6; meta1$xat  # meta1 is changed too
+##' meta2 = meta; meta2$xat = 2:6; meta$xat  # meta is changed too
 ##'
-##' meta1$posChanged$connect(function() {
-##' message('the mouse position is (', paste(meta1$pos, collapse = ','), ') now')
+##' meta$posChanged$connect(function() {
+##' message('the mouse position is (', paste(meta$pos, collapse = ','), ') now')
 ##' })
-##' meta1$pos = c(2, 0)
+##' meta$pos = c(2, 0)
+##'
+##' ls(meta)  # list all the objects in it
 ##' @author Yihui Xie <\url{http://yihui.name}>
+##' @export
 ##' @keywords internal
 Common.meta =
-    setRefClass("Common_meta",
-                fields = signalingFields(list(alpha = 'numeric',
-                xat = 'numeric', yat = 'numeric', xlab = 'character', ylab = 'character',
-                xlabels = 'character', ylabels = 'character', limits = 'matrix',
-                color = 'character', border = 'character', size = 'numeric',
-                start = 'numeric', pos = 'numeric',
-                brush.move = 'logical', brush.size = 'numeric',
-                manual.brush = 'function',
-                identified = 'integer', identify.labels = 'character')))
+    list(alpha = 'numeric', main = 'character',
+         xat = 'numeric', yat = 'numeric', xlab = 'character', ylab = 'character',
+         xlabels = 'character', ylabels = 'character', limits = 'matrix',
+         color = 'character', border = 'character', size = 'numeric',
+         start = 'numeric', pos = 'numeric',
+         brush.move = 'logical', brush.size = 'numeric',
+         manual.brush = 'function',
+         identified = 'integer', identify.labels = 'character')
