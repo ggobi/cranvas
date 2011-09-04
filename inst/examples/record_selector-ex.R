6b72e4fe2562b28c306a6395217cfaefee8c3389
Yihui Xie <xie@yihui.name>
2011-09-03 23:11:25 -0500

    roxygenize cranvas

---
+++ inst/examples/record_selector-ex.R
@@ -0,0 +1,14 @@
+library(cranvas)
+
+## old iris as the toy example
+qiris = qdata(iris)
+qparallel(~., data = qiris)
+record_selector(Species, data = qiris)
+
+## NRC rankings
+data(nrcstat)
+qnrc = qdata(nrcstat)
+qparallel(10:13, data = qnrc, main = 'Overview of Rankings', horizontal=FALSE)
+record_selector(Institution.Name, data = qnrc)
+qparallel(14:19, data = qnrc, main = 'Research, Student Support, Diversity')
+qparallel(20:26, data = qnrc, main = 'Publication, Award, Time to Degree')
