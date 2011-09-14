57a0807422e07fdfa731a60289a49600554eb30e
Yihui Xie <xie@yihui.name>
2011-09-14 00:45:09 -0500

    add examples of cartograms

---
+++ inst/examples/cart_polygon-ex.R
@@ -0,0 +1,13 @@
+library(cranvas)
+
+## a simple example: two polygons of size 1 and 5 respectively
+theta=seq(0,2*pi,length=100)
+x=c(cos(theta),NA,cos(theta)+2);y=c(sin(theta),NA,sin(theta)+1)
+plot(x,y,type='n',xlim=c(-2,5),ylim=c(-2,3)); polygon(x,y)
+text(c(0,2),c(0,1),c(1,5))  # original polygons
+
+res=cart_polygon(x, y, c(1,5), nrow=100,ncol=100)
+polygon(res$x,res$y,border='red',lty=2,lwd=2)  # transformed polygons
+
+
+## see map_qdata() for examples of real maps
