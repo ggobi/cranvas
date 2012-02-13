3ae5f540f064624b68369f401841164b8fd2d5c6
Yihui Xie <xie@yihui.name>
2012-02-12 21:39:37 -0600

    do not run the time-consuming wages example

--- inst/examples/wages-ex.R
+++ inst/examples/wages-ex.R
@@ -1,6 +1,7 @@
 library(cranvas)
 data(wages)
 
+\dontrun{
 ## this is a very simple linear regression assuming each man has the same slope, which may not be entirely reasonable
 fit = lm(lnw~exper+id,data=wages)
 wage2=subset(wages,!duplicated(wages$id), c(id, hispanic))
@@ -11,3 +12,4 @@ qwage2=qdata(wage2,color=hispanic)
 
 qhist(lnw0, main = 'Wages on the first day')
 qhist(lnw6, main = 'Wages after 6 years')
+}
