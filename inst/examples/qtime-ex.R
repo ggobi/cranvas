d5dfb0cac9c7cae0722fb94202cf7caaf4febaf4
Xiaoyue <chequered@gmail.com>
2012-02-19 22:06:28 -0600

    examples with the group setting

--- inst/examples/qtime-ex.R
+++ inst/examples/qtime-ex.R
@@ -13,11 +13,11 @@ qtime(TimeIndx,~ts+ps_tovs+ca_med,qnasa,wrap=FALSE)
 # qtime(TimeIndx,~ts+ps_tovs+ca_med,qnasa,Year)
 # qtime(TimeIndx,~ts+ps_tovs+ca_med,qnasa,Year,wrap=FALSE)
 
-library(reshape)
-nasaTsCa <- nasa11[,c(6,9,14)]
-nasaTsCa[,2:3] <- rescaler(nasaTsCa[,2:3])
-nasaTsCa <- melt(nasaTsCa,1)
-qnasaTsCa <- qdata(nasaTsCa)
+# library(reshape)
+# nasaTsCa <- nasa11[,c(6,9,14)]
+# nasaTsCa[,2:3] <- rescaler(nasaTsCa[,2:3])
+# nasaTsCa <- melt(nasaTsCa,1)
+# qnasaTsCa <- qdata(nasaTsCa)
 # qtime(TimeIndx,~value,qnasaTsCa,group=variable,shift=c(1,12))
 
 
@@ -33,30 +33,30 @@ qtime(Time,~conc,Rem)
 # qtime(Time,~conc,qRemi,group=ID,wrap=FALSE)
 
 # for categorical brushing self-link dataset by ID:
-id <- link_cat(qRemi, "ID")
-remove_link(qRemi, id)
+# id <- link_cat(qRemi, "ID")
+# remove_link(qRemi, id)
 
 
 ## example 3: Wages
-data(wages)
-wage <- qdata(wages[as.integer(as.character(wages$id))<2000,1:3])
+# data(wages)
+# wage <- qdata(wages[as.integer(as.character(wages$id))<2000,1:3])
 # qtime(exper,~lnw,wage,group=id)
-id <- link_cat(wage, "id")
-remove_link(wage, id)
+# id <- link_cat(wage, "id")
+# remove_link(wage, id)
 
 
 ## example 4: Lynx - for posterity
 # Good to show off wrapping to investigate irregular series
 data(lynx)
 qlynx<-qdata(data.frame(Time=1:114, lynx))
-# qtime(Time, ~lynx, qlynx, shift=1:12)
+qtime(Time, ~lynx, qlynx, shift=1:12)
 
 
 ## example 5: Sunspots - for posterity
 # Good to show off wrapping to investigate irregular series
 data(sunspots)
 qsun<-qdata(data.frame(Time=1:2820, sunspots))
-# qtime(Time, ~sunspots, qsun, shift=c(1,(1:10)*10))
+qtime(Time, ~sunspots, qsun, shift=c(1,(1:10)*10))
 
 
 ## example 6: Pigs
@@ -64,13 +64,12 @@ data(pigs)
 qpig<-qdata(pigs)
 qtime(TIME, ~GILTS+PROFIT+PRODUCTION+HERDSZ, qpig, shift=c(1,4))
 
-library(reshape)
-pigGP <- pigs[,c(1,7,8)]
-pigGP[,2:3] <- rescaler(pigGP[,2:3])
-pigGP <- melt(pigGP,1)
-qpigGP <- qdata(pigGP)
+# library(reshape)
+# pigGP <- pigs[,c(1,7,8)]
+# pigGP[,2:3] <- rescaler(pigGP[,2:3])
+# pigGP <- melt(pigGP,1)
+# qpigGP <- qdata(pigGP)
 # qtime(TIME,~value,qpigGP,group=variable,shift=c(1,4))
-
-id <- link_cat(qpigGP, "variable")
-remove_link(qpigGP, id)
+# id <- link_cat(qpigGP, "variable")
+# remove_link(qpigGP, id)
 
