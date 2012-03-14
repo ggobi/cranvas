25b855c2d26d74bc2e4cefd68b76b70a3fda38c3
Dianne Cook <visnut@gmail.com>
2012-03-14 11:40:55 -0500

    a couple more example lines

--- inst/examples/qtour-ex.R
+++ inst/examples/qtour-ex.R
@@ -7,7 +7,7 @@ flea_tour = qtour(1:6, data = qflea, tour_path = grand_tour(3))
 flea_tour$start()
 
 Sys.sleep(.5)  # need a pause to generate projections
-qscatter(proj1, proj2, data = qflea, xlim = c(-.8, .8), ylim = c(-.8, .8))
+qscatter(proj1, proj2, data = qflea, xlim = c(-.8, .8), ylim = c(-.8, .8), xlab="Projection 1", ylab="Projection 2")
 qparallel(~proj1+proj2+proj3, data = qflea)
 qhist(proj1, data = qflea, binwidth = 0.05, xlim = c(-1, 1), ylim = c(0, 10))
 
@@ -20,6 +20,10 @@ flea_tour$faster()
 
 ## change the type of tour
 flea_tour$tour_path = guided_tour(holes, d = 3)
+flea_tour$tour_path = grand_tour(d = 3)
+flea_tour$tour_path = guided_tour(lda_pp(flea$species), d = 3)
+flea_tour$tour_path = grand_tour(d = 3)
 
 ## do sphering
 flea_tour$sphere = TRUE
+
