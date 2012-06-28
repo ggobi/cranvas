eb320e02c1825a567804e59c89a02e671a2e9020
Yihui Xie <xie@yihui.name>
2012-06-28 11:40:53 -0400

    let col2rgb() report the possible error directly instead of let me do it

--- R/data.R
+++ R/data.R
@@ -71,11 +71,7 @@ qdata = function(data, color = "gray15", border = color, size = 4,
           pal = hue_pal(); dscale(v, pal)
         } else if (is.numeric(v)) {
           pal = seq_gradient_pal(); cscale(v, pal)
-        } else {
-          if (!inherits(try(col2rgb(v), silent = TRUE), 'try-error')) v else
-            stop(sQuote(i),
-                 ' must be either a factor or a numeric variable or valid colors!')
-        }
+        } else col2rgb(v)
       } else if (is.numeric(v)) {
         pal = (function(range = c(1, 6)) {
           function(x) scales::rescale(x, range, range(x, na.rm = TRUE))
