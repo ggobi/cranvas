c514be344a401c31d894df2e050e7d0d508c7f8d
Yihui Xie <xie@yihui.name>
2012-07-07 17:18:37 -0400

    simplify the implementation of qpar()

--- R/options.R
+++ R/options.R
@@ -12,29 +12,17 @@
 #' qpar(mar = 0.05)  # the degree to extend the plot margin (inner margin)
 #' qpar(op)  # restore
 qpar = function(...) {
-    p = list(...)
-    .opts = .cranvasEnv$.opts
-    if (length(p)) {
-        if (is.null(names(p)) && !is.list(p[[1]])) {
-            p = unlist(p)
-            if (length(p) == 1)
-                .opts[[p]]
-            else .opts[p]
-        }
-        else {
-            omf = .opts
-            if (is.list(p[[1]]))
-                p = p[[1]]
-            if (length(p) > 0) {
-                .opts[names(p)] = p
-                .cranvasEnv$.opts = .opts
-            }
-            invisible(omf)
-        }
-    }
-    else {
-        .opts
-    }
+  p = list(...); opts = .cranvasEnv$.opts
+  # get all options
+  if (!length(p)) return(opts)
+  # get specified options
+  if (is.null(names(p)) && !is.list(p[[1]])) {
+    p = unlist(p)
+    if (length(p) == 1L) opts[[p]] else opts[p]
+  } else {
+    if (is.list(p[[1]])) p = p[[1]]
+    .cranvasEnv$.opts[names(p)] = p
+  }
 }
 
 qpar(mar = 0.05)
