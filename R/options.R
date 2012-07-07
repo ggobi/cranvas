055ef922368707bea4c24e4cc32b53444fe1537b
Yihui Xie <xie@yihui.name>
2012-07-07 16:50:49 -0400

    use #'

--- R/options.R
+++ R/options.R
@@ -1,18 +1,16 @@
-##' Graphical parameters in cranvas
-##'
-##' This function can set or query the graphical parameters in the
-##' \code{cranvas} package.
-##'
-##' @param ... options of the form \code{tag = value}
-##' @return the current list of parameters or set new options
-##' @author Yihui Xie <\url{http://yihui.name}>
-##' @seealso \code{\link[graphics]{par}}
-##' @export
-##' @examples
-##' op = qpar()
-##' qpar(mar = 0.05)  # the degree to extend the plot margin (inner margin)
-##' qpar(op)  # restore
-##'
+#' Graphical parameters in cranvas
+#'
+#' This function can set or query the graphical parameters in the \pkg{cranvas}
+#' package.
+#' @param ... options of the form \code{tag = value}
+#' @return the current list of parameters or set new options
+#' @author Yihui Xie <\url{http://yihui.name}>
+#' @seealso \code{\link[graphics]{par}}
+#' @export
+#' @examples
+#' op = qpar()
+#' qpar(mar = 0.05)  # the degree to extend the plot margin (inner margin)
+#' qpar(op)  # restore
 qpar = function(...) {
     p = list(...)
     .opts = .cranvasEnv$.opts
