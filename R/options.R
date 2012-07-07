d93efee479fd53f8a720d7424d6f8406f080c1db
Yihui Xie <xie@yihui.name>
2012-07-07 17:43:18 -0400

    tweak doc of qpar()

--- R/options.R
+++ R/options.R
@@ -2,12 +2,23 @@
 #'
 #' This function can set or query the graphical parameters in the \pkg{cranvas}
 #' package.
+#'
+#' Current options include:
+#'
+#' \describe{
+#'
+#' \item{cache}{whether to enable cache for graphics layers}
+#'
+#' \item{mar}{a percentage for the plot margin}
+#'
+#' }
 #' @param ... options of the form \code{tag = value}
 #' @return the current list of parameters or set new options
 #' @author Yihui Xie <\url{http://yihui.name}>
 #' @seealso \code{\link[graphics]{par}}
 #' @export
 #' @examples
+#' str(qpar())  # all default parameters
 #' op = qpar()
 #' qpar(mar = 0.05)  # the degree to extend the plot margin (inner margin)
 #' qpar(op)  # restore
