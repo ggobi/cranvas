b8f766bcb157dff1eeb80d8a4963a79bef286ef2
Heike Hofmann <hofmann@iastate.edu>
2012-08-31 19:25:03 -0500

    title corrected

--- R/qmosaic.R
+++ R/qmosaic.R
@@ -32,7 +32,7 @@ find_x_label <- function(df) {
   vars <- setdiff(names(df), c(".wt", "l", "r", "t", "b", "level"))
   
   axis.set <- subset(df, (b==min(b)) &  (level==max(level)), drop=FALSE)
-  
+browser()  
   paste(vars[sapply(vars, function(x) return(length(unique(axis.set[,x]))>1))],"")
 }
 
@@ -40,7 +40,7 @@ find_y_label <- function(df) {
   vars <- setdiff(names(df), c(".wt", "l", "r", "t", "b", "level"))
   
   axis.set <- subset(df, (l==min(l)) & (level==max(level)), drop=FALSE)
-  
+browser()  
   paste(vars[sapply(vars, function(x) return(length(unique(axis.set[,x]))>1))],"")
 }
 
