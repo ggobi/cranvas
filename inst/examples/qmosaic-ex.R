48be24ab82c16736effe82c702c79bfe6e7f93ab
Heike Hofmann <hofmann@iastate.edu>
2012-09-04 19:15:46 -0500

    Simpson paradox in Titanic data

--- inst/examples/qmosaic-ex.R
+++ inst/examples/qmosaic-ex.R
@@ -1,6 +1,26 @@
 library(cranvas)
 library(productplots)
 
+
+## Simpson on board the Titanic?!
+data(Titanic)
+titanic <- as.data.frame(Titanic)
+titanic$Passenger <- titanic$Class != "Crew"
+qtitanic <- qdata(titanic)
+
+qtitanic$.brushed <- qtitanic$Survived=="Yes"
+## passengers have a higher probabiloty of survival than crew members ...
+print(qmosaic(qtitanic, Freq ~ Passenger, divider=c("hspine")))
+## unless sex is taken into account: the conditional association between Passenger 
+## and Survival changes to the opposite: both male and female crew members had better 
+## changes of survival than their counterparts on the passenger side.
+print(qmosaic(qtitanic, Freq ~ Passenger|Sex, divider=c("hspine", "hspine")))
+
+## this is an example of the Simpson's paradox, and is due to the strong association
+## between crew membership and gender.
+print(qmosaic(qtitanic, Freq ~ Sex+Passenger, divider=c("vspine", "hspine")))
+
+#######################
 data(happy)
 qhappy <- qdata(happy, color=happy)
 qbar(happy, qhappy)
@@ -11,11 +31,7 @@ qmosaic(qhappy, ~marital+degree, divider=c("vspine", "hspine"))
 
 qbar(sex, qhappy)
 
-data(Titanic)
-titanic <- as.data.frame(Titanic)
-qtitanic <- qdata(titanic)
 
-qtitanic$.brushed <- qtitanic$Survived=="Yes"
 print(qmosaic(qtitanic, Freq ~ Sex + Age + Class))
 print(qbar(Survived, data=qtitanic))
 
