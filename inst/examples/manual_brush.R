f689b02a2bf6698caf4a4a2948277c15dbb91a2a
Yihui Xie <xie@yihui.name>
2013-10-17 15:44:22 -0500

    fixes the manual brush problem in #208

--- inst/examples/manual_brush.R
+++ inst/examples/manual_brush.R
@@ -6,10 +6,10 @@ qnrc = qdata(nrcstat, color = RegCode)
 
 ## automatically brush the bar plot
 brush(qnrc, 'style')$color = 'brown'
-l = attr(o1$view, 'meta')$limits
+l = attr(o1, 'meta')$limits
 # interpolate some random positions
 p = approx(runif(50, l[1, 1], l[2, 1]), runif(50, l[1, 2], l[2, 2]), n = 500)
-manual_brush(o1$view, cbind(p$x, p$y), pause = 0.1)
+manual_brush(o1, cbind(p$x, p$y), pause = if (interactive()) 0.1 else 0)
 
 ## can also brush the par-coords plot
-manual_brush(o2$view, as.matrix(expand.grid(seq(0, 1, 0.1), (1:4)-.1)), pause = 0.2)
+manual_brush(o2, as.matrix(expand.grid(seq(0, 1, 0.1), (1:4)-.1)), pause = if (interactive()) 0.2 else 0)
