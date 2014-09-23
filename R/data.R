dc0be6a037c9c64a60a25c9c8821b608ede90dba
chxy <chequered@gmail.com>
2014-09-23 13:24:12 -0500

    think of the color blind

--- R/data.R
+++ R/data.R
@@ -66,6 +66,7 @@ qdata = function(data, color = "gray15", border = color, size = 4,
       pal = NULL
       mf[[sprintf('.%s', i)]] = if (i != 'size') {
         if (is.factor(v)) {
+          #pal = dichromat_pal("BluetoOrange.8"); na_colors(dscale(v, pal))
           pal = hue_pal(); na_colors(dscale(v, pal))
         } else if (is.numeric(v)) {
           pal = seq_gradient_pal(...); na_colors(cscale(v, pal))
